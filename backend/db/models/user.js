d277cbf917186d32571392f09a7792226ee7e958
yyjjjj <lyj1107@gist.ac.kr>
2020-10-10 10:47:31 +0900

    feat: users db 생성 및 sequelize 연결
    - mysql 을 이용하여 users db 생성
    - users sequelize 연결

---
+++ backend/db/models/user.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        email: {
+          type: Sequelize.STRING(256),
+          allowNull: false,
+          unique: true,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        name: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+        },
+        hash: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: 'User',
+        tableName: 'users',
+        paranoid: true,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      }
+    );
+  }
+};
