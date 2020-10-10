0d331b288aae97f472859ad7a618e2f93baf41b7
Jonghun Park <jonghun.park.194@gmail.com>
2020-10-11 05:43:09 +0900

    feat: 그룹 페이지 뷰 레이아웃 작업
    - 그룹 페이지 작업을 할 수 있게 레이아웃 작성

--- frontend/src/views/GroupPage.vue
+++ frontend/src/views/GroupPage.vue
@@ -1,45 +1,62 @@
 <template>
-  <div>
-    <FeedLine />
-
-    <flicking
-      :options="{ gap: 10, bound: true, moveType: 'freeScroll' }"
-      :tag="'div'"
-      :viewportTag="'div'"
-      :cameraTag="'div'"
-    >
-      <div>
-        <div>Contetns of panel 0</div>
-      </div>
-      <div>
-        <div>Contetns of panel 1</div>
-      </div>
-      <div>
-        <div>Contetns of panel 2</div>
-      </div>
-      <div>
-        <div>Contetns of panel 3</div>
-      </div>
-      <div>
-        <div>Contetns of panel 4</div>
-      </div>
-      <div>
-        <div>Contetns of panel 5</div>
-      </div>
-    </flicking>
-
-  </div>
+  <v-container>
+    <v-layout class="group-page justify-space-between" row>
+      <v-col cols="3">
+        <v-container class="my-feed feed-container">
+          <h1>My Feed</h1>
+          <FeedLine :feeds="myFeeds"/>
+        </v-container>
+      </v-col>
+      <v-col cols="3">
+        <v-container class="friend-feed feed-container">
+          <FeedLine />
+        </v-container>
+      </v-col>
+      <v-col cols="3">
+        <v-container class="friend-feed feed-container">
+          <FeedLine />
+        </v-container>
+      </v-col>
+      <v-col cols="3">
+        <v-container class="friend-feed feed-container">
+          <FeedLine />
+        </v-container>
+      </v-col>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 import FeedLine from '@/components/FeedLine.vue'
-import { Flicking } from '@egjs/vue-flicking'
 
 export default {
   name: 'GroupPage',
   components: {
-    FeedLine,
-    Flicking
-  }
+    FeedLine
+  },
+  data: () => ({
+    myFeeds: [
+      1,
+      2
+    ]
+  })
 }
 </script>
+
+<style>
+.group-page {
+}
+
+.feed-container {
+  height: 90vh;
+  border-radius: 16px;
+}
+
+.friend-feed {
+  background: #ECECEC;
+}
+
+.my-feed {
+  background: #EAEAFC;
+}
+</style>
