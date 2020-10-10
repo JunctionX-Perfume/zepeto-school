7b4e4c29f914eea202f0e72a4f724775ec124a50
최유라 <yura@MacBook-Pro.local>
2020-10-11 00:46:11 +0900

    Editor for new post

--- frontend/src/components/Posting.vue
+++ frontend/src/components/Posting.vue
@@ -1,14 +1,22 @@
 <template>
+  <v-row>
     <v-card elevation="2" outlined shaped>
         <v-text class="card" v-html="html"></v-text>
     </v-card>
+    <PostEditor/>
+  </v-row>
 </template>
 
 <script>
+import PostEditor from './PostEditor.vue'
+
 export default {
   name: 'Posting',
   props: {
     html: String
+  },
+  components: {
+    PostEditor
   },
   mounted () {
     const img = this.img
