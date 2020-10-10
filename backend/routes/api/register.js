5bb54390933f3aabda50cc9938e41b102447de6f
yyjjjj <lyj1107@gist.ac.kr>
2020-10-10 10:51:04 +0900

    feat: jwt 기반 회원가입, 로그인, 로그아웃, 테스트 라우터 추가
    - jwt 기반 회원가입, 로그인, 로그아웃 라우터 추가
    - 로그인 상태를 확인가능한 test 라우터 추가

---
+++ backend/routes/api/register.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+const { User } = require('../../db/models');
+
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+
+    if (user)
+      return res.status(400).json({ message: '이미 존재하는 아이디입니다.' });
+
+    await User.create({
+      email: req.body.email,
+      password: req.body.password,
+      name: req.body.name,
+      hash: req.body.hash,
+    });
+
+    return res.status(200).json({ message: '회원가입이 완료되었습니다.' });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ err: err.message });
+  }
+});
+
+module.exports = router;
