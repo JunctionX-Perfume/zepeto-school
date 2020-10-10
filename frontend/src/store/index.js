3f7897cf5868cdacb2931b63ad7edcad4e767a75
Jonghun Park <jonghun.park.194@gmail.com>
2020-10-11 05:13:10 +0900

    feat: 프론트 엔드 로그인 기능 구현 - 로그인과 회원가입 기능 구현함

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isLogin: false,
+    user: {
+    }
   },
   mutations: {
+    SET_IS_LOGIN: (state, payload) => {
+      state.isLogin = payload
+    },
+    SET_USER: (state, user) => {
+      state.user = user
+    }
+  },
+  getters: {
+    getIsLogin: state => state.isLogin
   },
   actions: {
+    tryLogin ({ commit }, params) {
+      return axios.get('http://172.17.10.137:3000/api/login', {
+        params
+      })
+        .then(res => {
+          if (res.data.user !== undefined) {
+            commit('SET_IS_LOGIN', true)
+            commit('SET_USER', res.data.user)
+            return true
+          } else {
+            return false
+          }
+        })
+    }
   },
   modules: {
   }
