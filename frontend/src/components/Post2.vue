be5fbd9ed3a9243376bf12b316f97ac4a287f942
yyjjjj <lyj1107@gist.ac.kr>
2020-10-11 10:06:25 +0900

    feat: Post component 추가
    - 새로운 Post Component 추가

---
+++ frontend/src/components/Post2.vue
@@ -0,0 +1,18 @@
+<template>
+<section class="post">
+  <div>
+      <img src='../assets/post.svg'/>
+      <div>JunctionX 2020 Seoul</div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'MyPage'
+}
+
+</script>
+
+<style scoped>
+</style>
